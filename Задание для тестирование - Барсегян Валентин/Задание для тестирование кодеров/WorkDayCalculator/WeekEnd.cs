58cbee7fd608672a9fcfab367f4fc01cefca3f39
Bars <bars92@hotmail.it>
2016-09-17 14:19:31 +0300

    Add files
    adding other files

---
+++ Задание для тестирование - Барсегян Валентин/Задание для тестирование кодеров/WorkDayCalculator/WeekEnd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WorkDayCalculatorProj
+{
+    public class WeekEnd
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public WeekEnd(DateTime startDate, DateTime endDate)
+        {
+            if(startDate > endDate) throw new ArgumentOutOfRangeException();
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
---
+++ Задание для тестирование - Барсегян Валентин/Задание для тестирование кодеров/WorkDayCalculator/WorkDayCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WorkDayCalculatorProj
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            if (dayCount < 0) throw new ArgumentOutOfRangeException();
+            if (dayCount == 0) return startDate;
+
+            if (weekEnds == null) return startDate.AddDays(dayCount);
+
+            //int allDays = dayCount;
+            foreach (var weekEnd in weekEnds)
+            {
+                if(weekEnd == null) continue;
+
+                if (weekEnd.StartDate > startDate && weekEnd.StartDate <= startDate.AddDays(dayCount)) //if first day of the weekend is in range of dayCount
+                {
+                    dayCount += (int)weekEnd.EndDate
+                        .Subtract(weekEnd.StartDate)
+                        .TotalDays;
+                    dayCount++;
+                }
+                else if (weekEnd.EndDate > startDate) //if last day of the weekened is in range of dayCount
+                {
+                    dayCount += (int)weekEnd.EndDate
+                        .Subtract(startDate)
+                        .TotalDays;
+                }
+            }
+
+            return startDate.AddDays(dayCount); 
+        }
+    }
+}
