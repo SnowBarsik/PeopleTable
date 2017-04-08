3cef80eb51c940ace3a6e0ce77bc747d5201b48b
Bars <bars92@hotmail.it>
2017-04-08 18:04:34 +0300

    delete folder

--- Задание для тестирование - Барсегян Валентин/Задание для тестирование кодеров/WorkDayCalculator/IWorkDayCalculator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace WorkDayCalculatorProj
-{
-    public interface IWorkDayCalculator
-    {
-        DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds);
-    }
-}
